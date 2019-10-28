27c8d8eec9e042ce9590cc3382d9ab145580c766
TomasKostadinov <tomas.kostadinov@conclurer.com>
2019-10-28 13:43:14 +0100

    Change port to automatically use specified port by heroku

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 4000));
 app.use(express.static('dist'));
 app.listen();
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+});
